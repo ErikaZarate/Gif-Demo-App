30d3758aa4d5f66878d8178ef6bf56e13bb1145e
Erika Zarate Gutierrez <erikazg20@gmail.com>
2023-05-25 09:30:14 -0600

    Avance agregar

---
+++ src/components/GifsExpo/index.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react"
+
+const GifsExpo = ({categories = []}) => {
+
+  const [urlList, setUrlList] = useState([])
+
+    const getGifs = async (categories) => {
+
+      const responsesList = await Promise.all(categories.map(async (category) =>{
+        const response = await fetch(
+          `https://api.giphy.com/v1/gifs/search?api_key=C4kTA04UeVeljjVeBkLGaOTYrGYuKyFE&q=${category}&limit=10`
+        )
+        const apiResponse = await response.json()
+        return apiResponse.data
+      }))
+
+      let gifsList = []
+
+      responsesList.forEach((data) => {
+        data.forEach((item) => {
+          gifsList = [...gifsList, item.images.fixed_height.url.split('?')[0]]
+        })
+      })
+
+      setUrlList([...gifsList])
+    }
+    getGifs(categories)
+
+  return (
+    <>
+      <div>
+        {
+          urlList.map((url) => {
+            return (
+              <img key={url} src={url}/>
+            )
+          })
+        }
+      </div>
+    </>
+  )
+}
+
+export default GifsExpo
